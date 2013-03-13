749b1bc4285721cb05be10bf368dbbfa051e6da9
Sam Lester <hello@samlester.net>
2013-03-13 00:42:19 +0000

    Put nav into a seperate file

---
+++ includes/nav.php
@@ -0,0 +1,19 @@
+<div class="navbar navbar-static-top">
+	<div class="container">
+		<a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</a>
+		<a class="navbar-brand" href="#">Scoop</a>
+		<ul class="nav nav-collapse collapse navbar-responsive-collapse">
+			<li><a href="#">Projects</a></li>
+			<li><a href="#">Reports</a></li>
+			<li><a href="#">Users</a></li>
+		</ul>
+		<ul class="nav nav-collapse collapse navbar-responsive-collapse pull-right">
+			<li><p>Hello Sam!</p></li>
+			<li><a href="#">Sign out</a></li>
+		</ul>
+	</div>
+</div>
--- site-listing.php
+++ site-listing.php
@@ -1,4 +1,5 @@
 <?php require_once('includes/header.php'); ?>
+<?php require_once('includes/nav.php'); ?>
 
 <div class="scoop-pageheader">
 <div class="container">
