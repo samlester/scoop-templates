ba3cf99757b301804888c9c5e0ebbf8595beae97
Sam Lester <hello@samlester.net>
2013-03-13 00:49:08 +0000

    Sign in page

---
+++ sign-in.php
@@ -0,0 +1,16 @@
+<?php require_once('includes/header.php'); ?>
+
+<div class="container">
+	<div class="row">
+		<div class="span4 offset4 login">
+			<input type="text" placeholder="Email address">
+			<input type="password" placeholder="Password">
+			<label class="checkbox">
+				<input type="checkbox">  Remember me
+			</label>
+			<button type="submit" class="btn btn-primary">Sign in</button>
+		</div>
+	</div>
+</div>
+
+<?php require_once('includes/footer.php'); ?>
